6317d1f95f2f7d7c4b930df229997d760dc0ecf6
jonatan <jonacastillo227@gmail.com>
2024-11-30 02:30:05 -0300

    el login esta terminado funciona tanto el login y usuario

---
+++ gestorUser/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from rest_framework.reverse import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render
+
+# Create your views here.
+@login_required
+def index(request):
+    return render(request, "index.html")
+
+
+class SignUpView(SuccessMessageMixin, CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+    success_message = "¡Usuario creado exitosamente!"
