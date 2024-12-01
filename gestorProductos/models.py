c519e6ef7f1be35c21061d7459a73e17ec0e58a5
jonatan <jonacastillo227@gmail.com>
2024-12-01 03:00:15 -0300

    se cambio el models.py y agregue urls.py pero lo deje vacio por errores.

--- gestorProductos/models.py
+++ gestorProductos/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
-# modelo de los productos
-class Productos(models.Model):
+class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=200)
-    categoria = models.CharField(max_length=100)
-    precio = models.IntegerField()    
-    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
