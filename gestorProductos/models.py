cc95e62cf9a15555788594a9a5b1a7d2401b2c90
Juan <juanjosereyesherrera@gmail.com>
2024-12-01 15:44:44 -0300

    auxilio

--- gestorProductos/models.py
+++ gestorProductos/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 # from gestorUser.models import CustomUser  
 
-<<<<<<< HEAD
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
@@ -15,12 +14,6 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
    # creador = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-=======
-class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
->>>>>>> ca89f90409cb29469cfc783817dfce9d0b2e1888
 
     def __str__(self):
         return self.nombre
