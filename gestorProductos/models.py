3e310b70c59d2d871b2866a10403c3c5c702ebb5
Juan <juanjosereyesherrera@gmail.com>
2024-12-01 02:47:30 -0300

    simplificación de models

--- gestorProductos/models.py
+++ gestorProductos/models.py
@@ -4,6 +4,4 @@ from django.db import models
 class Productos(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=200)
-    categoria = models.CharField(max_length=100)
     precio = models.IntegerField()    
-    stock = models.IntegerField()
