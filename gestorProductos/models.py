c7ab123cecd28943afd2488d47684d035c478fb7
Juan <juanjosereyesherrera@gmail.com>
2024-12-01 02:31:14 -0300

    modelo de productos

--- gestorProductos/models.py
+++ gestorProductos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# modelo de los productos
+class Productos(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=200)
+    categoria = models.CharField(max_length=100)
+    precio = models.IntegerField()    
+    stock = models.IntegerField()
