ca89f90409cb29469cfc783817dfce9d0b2e1888
jonatan <jonacastillo227@gmail.com>
2024-12-01 04:20:47 -0300

    agrege un texto centrado

--- gestorProductos/models.py
+++ gestorProductos/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 
-class Categoria(models.Model):
+class Producto(models.Model):
     nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.nombre
-
-class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=200)
-    categoria = models.CharField(max_length=100)
-    precio = models.IntegerField()    
-    stock = models.IntegerField()
